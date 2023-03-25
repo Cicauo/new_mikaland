7db066a764959be3051d5236ade297d08ccc166c
nifist99 <nikofigit@gmail.com>
2023-03-25 22:11:23 +0700

    fixing

--- resources/views/admin/management/artikel/create.blade.php
+++ resources/views/admin/management/artikel/create.blade.php
@@ -60,10 +60,11 @@ integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLAS
 @push('js')
 
  <!-- TinyMCE -->
- <script src="https://cdn.tiny.cloud/1/0el4n01c6kp1p847l54ghx1wkxjadb3pe3p1bk1orbvnq5wx/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
  <script>
+
+<script src="https://cdn.tiny.cloud/1/0el4n01c6kp1p847l54ghx1wkxjadb3pe3p1bk1orbvnq5wx/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 tinymce.init({
   selector: 'textarea#content',
   height: 500,
@@ -80,6 +81,7 @@ tinymce.init({
 });
 
  </script>
+
 
 @endpush
 
